43bf24dee938882683cb5d860f42b41188f124b2
Gabri <gabriel.fernandez.moral@gmail.com>
2016-03-05 15:06:43 +0100

    Trying to let heroku choose port on deploy

--- webserver.js
+++ webserver.js
@@ -1,6 +1,6 @@
 var connect = require('connect'),
 	serveStatic = require('serve-static');
 
-connect().use(serveStatic(__dirname)).listen();
+connect().use(serveStatic(__dirname)).listen(process.env.PORT || 3030);
 
 console.log('Listening...');
