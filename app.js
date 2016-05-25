901b619a594f1f6fdd68fc50017fbd08b6962bdd
Gabri <gabriel.fernandez.moral@gmail.com>
2016-05-25 21:14:04 +0200

    Try to avoid cors issues in production

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ app.config(['$routeProvider', '$sceDelegateProvider',
     			// Allow same origin resource loads.
     			'self',
     			// Allow loading from our assets domain.  Notice the difference between * and **.
-    			'http://localhost:3000/**'
+    			remoteServer + '**'
   		]);
 }]);
